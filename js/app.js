f6028298c28c8b9c8f7dcb107bff350ffbfd70ff
Mikołaj Dyczkowski <m.dyczkowski@outlook.com>
2018-03-28 18:22:13 +0200

    Added card matching logic.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,37 @@ const openCards = [];
 const cardGrid = document.querySelector('.card-grid');
 
 //
-// -----------> Card flipping and selecting on click
+// -----------> Comparing selected cards
 //
-
+function areMatching() {
+  if (openCards[0].textContent === openCards[1].textContent) {
+    openCards[0].classList.add('card--match');
+    openCards[1].classList.add('card--match');
+  } else {
+    openCards[0].classList.add('card--nomatch');
+    openCards[1].classList.add('card--nomatch');
+    return false;
+  }
+  return true;
+}
+//
+// ------------> On click card selecting and flipping
+//
 cardGrid.addEventListener('click', (event) => {
   if (openCards.length < 2) {
+    if (event.target.classList.contains('card-grid')) { // Not allowing clicks on grid (or already matched cards)
+      return;
+    }
     event.target.classList.toggle('card--open');
-    openCards.push(event.target);
+    if (event.target.classList.contains('card--open')) openCards.push(event.target);
+    else openCards.pop();
+    if (openCards.length === 2) {
+      areMatching();
+      setTimeout(() => {
+        openCards[0].classList.remove('card--open', 'card--nomatch');
+        openCards[1].classList.remove('card--open', 'card--nomatch');
+        openCards.length = 0;
+      }, 1500); // clearing the array and closing cards
+    }
   }
 });
